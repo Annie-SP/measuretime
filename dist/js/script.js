153163b32e46790fab34271995cc3e5c6a5afdff
Annie Pylypchuk <anechkaaaaaaa@gmail.com>
2022-12-16 10:27:38 +0200

    TimeDifference in min, sec, h, days

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,102 +1,80 @@
-
+'use strict';
 
 const form = document.getElementById('countform');
 const buttonClick = document.querySelector('#button_count');
+//об'являємо section щоб привязати клас до першого елементу
+const result = document.getElementById('result');
+const resultDiv = document.querySelector('result_d');
+//обявляємо divи де будуть результати
+const resultMeasure = document.getElementById('resultMeasure');
+const resultDate1 = document.getElementById('resultDate1');
+const resultDate2 = document.getElementById('resultDate2');
+//коли ми натискаємо на кнопку
+//додати дані в локал сторадж
+
+
+//очистити дані з кнопок, після виведення результату в таблицю
+
+
+//вибрати проміжок часу
+
+
+//вибрати дні
+// let typeTimes = document.querySelector('period__block_input');
+
+// typeTimes.addEventListener('checked')
+
 
 form.addEventListener('submit', (event) => {
     // stop form submission
     event.preventDefault();
 });
 
-let date1 = document.querySelector('#firstDate');
-// Handle date changes
-firstDate.addEventListener('input', function () {
-  // Get the date
-  let firstDate = new Date(date1.value);
-  console.log(firstDate);
-});
+function TimeDifference() {
+    //define two variables and fetch the input from HTML form  
+     let date1 = document.getElementById("firstDate").value;  
+     let date2 = document.getElementById("lastDate").value;  
 
-let date2 = document.querySelector('#lastDate');
-// Handle date changes
-lastDate.addEventListener('input', function () {
-  // Get the date
-  let lastDate = new Date(date2.value);
-  console.log(lastDate);
-});
+    //define two date object variables to store the date values  
+    date1 = Date.parse(date1);
+    localStorage.setItem('Date1', new Date(date1.value));
+    date2 = Date.parse(date2);
+    localStorage.setItem('Date2', new Date(date2.value));
 
-buttonClick.addEventListener('input', function () {
-    //let difference = firstDate - lastDate;
-    console.log( firstDate - lastDate);
-});
+    let difference = Math.abs((date2) - (date1));
+    let secondsTime = difference / 1000; 
+    let minutesTime = difference / (1000 * 60); 
+    let hoursTime = difference / (1000 * 3600); 
+    let daysTime = difference / (1000 * 3600 * 24); 
+    let selectBlock = document.getElementById("act__block");
+    let selectedValue = selectBlock.options[selectBlock.selectedIndex].value;
+    console.log(selectedValue);
+    let output;
+    console.log("тип ", typeof(output));
+    if (selectedValue === 'seconds') {
+        output = secondsTime + ' seconds';
+    } else if (selectedValue === 'minutes') {
+        output = minutesTime  + ' minutes';
+    } else if (selectedValue === 'hours') {
+        output = hoursTime + ' hours';
+    } else if (selectedValue === 'days') {
+        output = daysTime + ' days';
+    };  
+    console.log(output);
+    localStorage.setItem('timeBetween', output)
+//повертає в result пораховані дані
+    resultMeasure.innerHTML = output;
+    resultDate1.innerHTML  = firstDate.value;
+    resultDate2.innerHTML = lastDate.value;
 
+};
+
+buttonClick.addEventListener('click', function () {
+    // event.preventDefault();
+    result.classList.add('result_block');
+    TimeDifference();
+    
+});
 
 // let monthSelect = document.querySelector('#month');
 // let weekSelect = document.querySelector('#week');
-
-
-//об'являємо section щоб привязати клас до першого елементу
-const result = document.querySelector('result');
-const resultDiv = document.querySelector('result_d');
-//обявляємо divи де будуть результати
-const resultMeasure = document.querySelector('resultMeasure');
-const resultDate1 = document.querySelector('resultDate1');
-const resultDate2 = document.querySelector('resultDate2');
-//коли ми натискаємо на кнопку
-
-
-    buttonClick.addEventListener('input', count);
-    //об'являємо кнопку
-
-
-    function count() {
-    // resultDiv.ClassList.remove('result_d');
-        resultDiv.ClassList.add('result_block');
-        console.log("class add");
-        //повертає в result пораховані дані
-        resultDate1.innerHTML  = firstDate.value;
-        resultDate2.innerHTML = Date2.value;
-    };
-
-    // (function() {
-    //     document.querySelector("#startDate").valueAsDate = new Date();
-    //     document.querySelector("#endDate").valueAsDate = new Date();
-    //     document.querySelector("#endDate").min = new Date()
-    //       .toISOString()
-    //       .substr(0, 10);
-    //     document.querySelector("#startDate").addEventListener("change", function() {
-    //       var val = this.value;
-    //       document.querySelector("#endDate").min = new Date(val)
-    //         .toISOString()
-    //         .substr(0, 10);
-    //     });
-    //   })();
-
-
-//       var a = document.getElementById("first").value;
-// var b = document.getElementById("second").value;
-// var firstDate = a.split('/');
-// var secondDate = b.split('/');
-// var n = new Date(firstDate[2], firstDate[0]-1, firstDate[1]);
-// var p = new Date(secondDate[2], secondDate[0]-1, secondDate[1]);
-// console.log("result: "+ (p-n)/(1000*60*60*24) );
-
-// function durationBetweenDates (start, end, typeTimes) {
-
-//     let firstDate = new Date(start);
-//     let lastDate = new Date(end);
-    
-    
-//     let difference = Math.abs((lastDate) - (firstDate));
-    
-//     if (typeTimes === 'seconds') {
-//         return (difference / 1000 ) + ' seconds';
-//     } else if (typeTimes === 'minutes') {
-//         return  (difference / (1000*60) )  + ' minutes';
-//     } else if (typeTimes === 'hours') {
-//         return (difference / (1000 * 3600)) + ' hours';
-//     } else if (typeTimes === 'days') {
-//         return (difference / (1000 * 3600 * 24)) + ' days';
-//     };
-        
-//     };
-    
