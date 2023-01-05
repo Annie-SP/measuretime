7248cd56c8f67c08f433a4d7dc07dde625e622bc
Annie Pylypchuk <anechkaaaaaaa@gmail.com>
2023-01-05 02:49:27 +0200

    date 7, date 30, localStorage, all days, min, sec, hours

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,246 +1,241 @@
-'use strict';
 
-const form = document.getElementById('countform');
+document.addEventListener('DOMContentLoaded', loadDates);
 const buttonClick = document.querySelector('#button_count');
+const form = document.getElementById('countform');
+
 //об'являємо section щоб привязати клас до першого елементу
 const result = document.getElementById('result');
-const resultDiv = document.querySelector('result_d');
 //обявляємо divи де будуть результати
-const resultMeasure = document.getElementById('resultMeasure');
-const resultDate1 = document.getElementById('resultDate1');
-const resultDate2 = document.getElementById('resultDate2');
-//коли ми натискаємо на кнопку
+const resultMeasure = document.getElementById('result_measure__list');
+const resultStartDate = document.getElementById('result_date1__list');
+const resultLastDate = document.getElementById('result_date2__list');
+
 //додати дані в локал сторадж
-let selectedTime;
+let selectedTime = document.querySelectorAll('period__block_name');
+let output;
 
-//очистити дані з кнопок, після виведення результату в таблицю
-
-//вибрати проміжок часу
-
-
-//вибрати дні
-// function periodTimes(){
-//     let typeTimes = document.querySelectorAll('period__block_input');
-
-//     for (let i = 0; i < typeTimes.length; i++)
-//         if (typeTimes[i].checked){
-//             period = typeTimes[i].value;
-//             break;
-//         }
-//         console.log(period);
-// }
-
-form.addEventListener('submit', (event) => {
-    // stop form submission
-    event.preventDefault();
-});
-
-let date1 = document.getElementById("firstDate");  
+//об'являємо дати
+let date1 = document.getElementById("firstDate"); 
 let date2 = document.getElementById("lastDate"); 
 
-date1.addEventListener('change', ()=> {
-    date1 = new Date(date1.value);
-            console.log('date1value', date1);
-            
-            date1 = new Date( date1.setDate(date1.getDate())); 
-            console.log('date1value+7', date1);
-            date2.value = null;
-            date2.valueAsDate = date1;
-            date2.setAttribute("min", date2.valueAsDate);
+date1.addEventListener('submit', createStartDate);
+
+// date2.addEventListener('change', periodSelect());
+
+
+buttonClick.addEventListener('click', function (event) {
+    event.preventDefault();
+    TimeDifference(); 
+    createStartDate();
+    storeStartDateInLocalStorage();
 });
+
+
+buttonClick.addEventListener('dblclick', function (event) {
+     localStorage.removeItem('startDate');
+     localStorage.removeItem('lastDate');
+     localStorage.removeItem('result');
+
+});
+
+function loadDates() {
+    // оголошуємо змінну яка буде використовуватись для списку завдань
+    let firstDates;
+    let lastDates;
+    let resultTime;
+
+    // перевіряємо чи є у ЛокалСтораджі вже які данні завдань
+    if(localStorage.getItem('startDate') !== null || localStorage.getItem('lastDate') !== null || localStorage.getItem('result') !== null) {
+        // якщо вони там є - витягуємо їх і присвоюємо змінній
+        firstDates = JSON.parse(localStorage.getItem('startDate'));
+        lastDates = JSON.parse(localStorage.getItem('lastDate'));
+        resultTime = JSON.parse(localStorage.getItem('result'));
+    } else {
+        // якщо їх там нема - присвоюємо змінній значення порожнього масиву
+        firstDates = [];
+        lastDates = [];
+        resultTime = [];
+    }
+
+    // для кожної задачі яка є Date1
+    firstDates.forEach(function(data1) {
+        // створюємо елемент списку
+        const li = document.createElement('li');
+        // додаємо йому класс
+        li.className = 'result__item_start';
+        // всередині цього елементу списку створюємо текстову ноду з описом завдання
+        li.appendChild(document.createTextNode(data1));
+        // запихуємо цей елемент списку в список
+        resultStartDate.appendChild(li);
+    })
+        // для кожної задачі яка є Date2
+    lastDates.forEach(function(data2) {
+        // створюємо елемент списку
+        const li = document.createElement('li');
+        // додаємо йому класс
+        li.className = 'result__item_finish';
+        // всередині цього елементу списку створюємо текстову ноду з описом завдання
+        li.appendChild(document.createTextNode(data2));
+        // запихуємо цей елемент списку в список
+        resultLastDate.appendChild(li);
+    })
+        // для кожної задачі яка є Result
+        resultTime.forEach(function(output) {
+        // створюємо елемент списку
+        const li = document.createElement('li');
+        // додаємо йому класс
+        li.className = 'result__item_measure';
+        // всередині цього елементу списку створюємо текстову ноду з описом завдання
+        li.appendChild(document.createTextNode(output));
+        // запихуємо цей елемент списку в список
+        resultMeasure.appendChild(li);
+    })
+}
+
+function createStartDate(event) {
+    // якщо значення в інпуті порожнє  - то не додаємо нове завдання і не даємо виконатись дефолтній поведінці
+    if(date1.value.trim() === '' || date2.value.trim() === ''|| output.trim() === '') {
+        event.preventDefault();
+        return null;
+    }
+
+    // створюємо елемент списку
+    let li = document.createElement('li');
+    let liLast = document.createElement('li');
+    let liResult = document.createElement('li');
+    // додаємо йому класс
+    li.className = 'result__item_start';
+    liLast.className = 'result__item_finish';
+    liResult.className = 'result__item_measure';
+    // всередині цього елементу списку створюємо текстову ноду з описом завдання
+    li.appendChild(document.createTextNode(date1.value));
+    liLast.appendChild(document.createTextNode(date2.value));
+    liResult.appendChild(document.createTextNode(output));
+    // запихуємо цей елемент списку в список
+    resultStartDate.appendChild(li);
+    resultLastDate.appendChild(liLast);
+    resultMeasure.appendChild(liResult);
+    // викликаємо функцію яка буде додавати завдання до ЛокалСтораджа
+    storeStartDateInLocalStorage(new Date(date1.value), new Date(date2.value), output);
+
+    // очищуємо вміст інпуту для створення завдання
+    date1.value = '';
+    date2.value = '';
+    // // блокуємо дефолтну поведінку сабміта
+    event.preventDefault();
+}
+
+function storeStartDateInLocalStorage(data1, data2, output ) {
+    // оголошуємо змінну яка буде використовуватись для списку завдань
+    let firstDates;
+    let lastDates;
+    let resultTime;
+
+    // перевіряємо чи є у ЛокалСтораджі вже які данні завдань
+    if(localStorage.getItem('startDate') !== null || localStorage.getItem('lastDate') !== null || localStorage.getItem('result') !== null) {
+        // якщо вони там є - витягуємо їх і присвоюємо змінній
+        firstDates = JSON.parse(localStorage.getItem('startDate'));
+        lastDates = JSON.parse(localStorage.getItem('lastDate'));
+        resultTime = JSON.parse(localStorage.getItem('result'));
+    } else {
+        // якщо їх там нема - присвоюємо змінній значення порожнього масиву
+        firstDates = [];
+        lastDates = [];
+        resultTime = [];
+    }
+
+    data1 = document.getElementById("firstDate").value;
+    data2 = document.getElementById("lastDate").value;
+    // output = output;
+    // додаємо до списку нове завдання
+    firstDates.push(data1);
+    lastDates.push(data2);
+    resultTime.push(output);
+    // зберігаємо список завданнь в ЛокалСТорадж
+    localStorage.setItem('startDate', JSON.stringify(firstDates));
+    localStorage.setItem('lastDate', JSON.stringify(lastDates));
+    localStorage.setItem('result', JSON.stringify(resultTime));
+}
+
+
+
+//function TimeDifference
+function TimeDifference() {
+    // periodSelect();
+    const firstDate = new Date(date1.value);
+    console.log(firstDate);
+        
+    const lastDate = new Date(date2.value);
+    console.log(lastDate);
+    //різницю між датами
+    let difference = Math.abs((lastDate) - (firstDate));
+    console.log(difference);
+    let secondsTime = difference / 1000; 
+    let minutesTime = difference / (1000 * 60); 
+    let hoursTime = difference / (1000 * 3600); 
+    let daysTime = difference / (1000 * 3600 * 24);   
+    let selectBlock = document.getElementById("act__block");
+    let selectedValue = selectBlock.options[selectBlock.selectedIndex].value;
+    console.log(selectedValue);
+
+
+    if (selectedValue === 'seconds') {
+        output = secondsTime + ' seconds';
+    } else if (selectedValue === 'minutes') {
+        output = minutesTime  + ' minutes';
+    } else if (selectedValue === 'hours') {
+        output = hoursTime + ' hours';
+    } else if (selectedValue === 'days') {
+        output = daysTime + ' days';
+    }; 
+    console.log(output);
+}
+
+
+
+function daysSelected(){
+    //в залежності від того який тип виведе різний результат
+    if (selectedTime === "alldays") {
+      TimeDifference();
+    }  
+   // ще не працює  
+    if (selectedTime === "weekends") {
+
+// const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+// let day = new Date().getDay(); //today
+// document.getElementById('addDay').addEventListener('click', function() {
+//   document.getElementById("demo").innerHTML = days[day++ % 7]; // move the ++ to ++day to show tomorrow on first click
+// })
+        TimeDifference();
+    } 
+    if (selectedTime === "workdays") {       
+      TimeDifference();
+    }    
+}
+
+
+date2.addEventListener('change', periodSelect());
 
 function periodSelect(){
     let btnMonth = document.getElementById("period__month");
     let btnWeek = document.getElementById("period__week");
 
           btnMonth.addEventListener('click', function(){
-            date1 = new Date(date1.value);
-            console.log('date1value', date1);
-            
-            date1 = new Date( date1.setDate(date1.getDate() + 30)); 
-            console.log('date1value+30', date1);
+
+            date3 = new Date(date1.value);
+            date3 = new Date( date3.setDate(date3.getDate() + 30)); 
             date2.value = null;
-            date2.valueAsDate = date1;
-            //date2.value = date2; 
-            console.log('date2', date2);
-            console.log('date2', date2.value);
+            date2.valueAsDate = date3;
+
         });
 
         btnWeek.addEventListener('click', function(){
-            date1 = new Date(date1.value);
-            console.log('date1value', date1);
-            
-            date1 = new Date( date1.setDate(date1.getDate() + 7)); 
-            console.log('date1value+7', date1);
+
+            date3 = new Date(date1.value);
+            date3 = new Date( date3.setDate(date3.getDate() + 7)); 
             date2.value = null;
-            date2.valueAsDate = date1;
+            date2.valueAsDate = date3;
         });
-
-// active second date only after first
-date2.setAttribute("disabled", "true"); 
-
-    date1.onclick = function(e)
-    {
-        if(e.target == date1)
-        {
-            date2.removeAttribute("disabled");
-        }
-        else
-            date2.setAttribute("disabled", "true");
-          
-    };
-
 }
 
-date1.addEventListener('click', periodSelect());
 
-
-
-//function TimeDifference
-
-function TimeDifference() {
-    // //об'являємо дати 
-    // let date1 = document.getElementById("firstDate");  
-    // let date2 = document.getElementById("lastDate");  
-   // date2.setAttribute("disabled", "true");
-
-    //переобразуємо їх
-    date1 = Date.parse(date1.value);
-    localStorage.setItem('Date1', new Date(date1));
-    date2 = Date.parse(date2.value);
-    localStorage.setItem('Date2', new Date(date2));
-    //різницю між датами
-    let difference = Math.abs((date2) - (date1));
-    let secondsTime = difference / 1000; 
-    let minutesTime = difference / (1000 * 60); 
-    let hoursTime = difference / (1000 * 3600); 
-    let daysTime = difference / (1000 * 3600 * 24); 
-    let selectBlock = document.getElementById("act__block");
-    let selectedValue = selectBlock.options[selectBlock.selectedIndex].value;
-    console.log(selectedValue);
-    let output;
-    console.log("тип ", typeof(output));
-    timeSelect();
-    //в залежності від того який тип виведе різний результат
-    if (selectedTime === "alldays") {
-        if (selectedValue === 'seconds') {
-            output = secondsTime + ' seconds';
-        } else if (selectedValue === 'minutes') {
-            output = minutesTime  + ' minutes';
-        } else if (selectedValue === 'hours') {
-            output = hoursTime + ' hours';
-        } else if (selectedValue === 'days') {
-            output = daysTime + ' days';
-        };  
-    }  
-   // ще не працює  
-    if (selectedTime === "weekends") {
-        if (selectedValue === 'seconds') {
-            output = secondsTime + ' seconds';
-        } else if (selectedValue === 'minutes') {
-            output = minutesTime  + ' minutes';
-        } else if (selectedValue === 'hours') {
-            output = hoursTime + ' hours';
-        } else if (selectedValue === 'days') {
-            output = daysTime + ' days';
-        };  
-    } 
-    if (selectedTime === "workdays") {
-       
-        if (selectedValue === 'seconds') {
-            output = secondsTime + ' seconds';
-        } else if (selectedValue === 'minutes') {
-            output = minutesTime  + ' minutes';
-        } else if (selectedValue === 'hours') {
-            output = hoursTime + ' hours';
-        } else if (selectedValue === 'days') {
-            output = outputDays + ' days';
-        };  
-    }    
-    console.log(output);
-    localStorage.setItem('timeBetween', output)
-//повертає в result пораховані дані
-    resultMeasure.innerHTML = output;
-    resultDate1.innerHTML  = firstDate.value;
-    resultDate2.innerHTML = lastDate.value;
-     
-};
-
-// let dayWeek = [7, 1, 2, 3, 4, 5, 6][date1.getDay()];
-// console.log(dayWeek);
-
-
-//
-// function getWeekDay(date) {
-//     let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-//   if (days[0] )
-//     return days[date.getDay()] - days[date.getDay()];
-//   }
-
-
-// let days = [
-//     'Неділя',
-//     'Понеділок',
-//     'Вівторок',
-//     'Середа',
-//     'Четвер',
-//     'Пятниця',
-//     'Субота'
-//   ];
-//   var d = new Date(date1);
-//   var n = d.getDay();
-//   console.log(days[n]);
-
-//without weekends
-// function workDays(){
-//     const daysWithOutWeekEnd = [];
-//     for (let currentDate = new Date(date1); 
-//     currentDate <= date2; 
-//     currentDate.setDate(currentDate.getDate() + 1)) {
-//     console.log('currentDate', currentDate);
-//     if (currentDate.getDay() != 0 && currentDate.getDay() != 6) {
-//         daysWithOutWeekEnd.push(new Date(currentDate));
-//     }
-//     }
-//     daysWithOutWeekEnd.length-1;
-//     console.log( daysWithOutWeekEnd.length-1);
-// }
-// let outputDays = workDays();
-// console.log( 'outputDays', outputDays);
-
-
-const timeRadioButtons = document.querySelectorAll('input[name="typeTime"]');
-
-function timeSelect(){
-    for (const timeRadioButton of timeRadioButtons) {
-        if (timeRadioButton.checked) {
-            selectedTime = timeRadioButton.value;
-            break;
-         }
-     }
-}
-
-buttonClick.addEventListener('click', function () {
-    // event.preventDefault();
-    result.classList.add('result_block');
-    TimeDifference();
-    // periodTimes();
-    
-});
-
-// let monthSelect = document.querySelector('#month');
-// let weekSelect = document.querySelector('#week');
-
-
-// const firstDate = new Date("December 16, 2022");
-// const secondDate = new Date("December 21, 2022");
-// const daysWithOutWeekEnd = [];
-// for (var currentDate = new Date(firstDate); 
-// currentDate <= secondDate; 
-// currentDate.setDate(currentDate.getDate() + 1)) {
-//    console.log(currentDate);
-//   if (currentDate.getDay() != 0 && currentDate.getDay() != 6) {
-//     daysWithOutWeekEnd.push(new Date(currentDate));
-//   }
-// }
-// console.log( daysWithOutWeekEnd.length-1);
